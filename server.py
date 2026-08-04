b25c1f65d5203465061a08d8855eecabff99991f
BCPMcustom <bcpmcustomphone@gmail.com>
2026-08-04 12:44:53 -0400

    API is working

--- server.py
+++ server.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, request 
 from SentimentAnalysis.sentiment_analysis import sentiment_analyzer 
 
-app = Flask("Sentiment Analyzer")
+app = Flask("sentimentAnalyzer")
 
-@app.route("/sentimentAnalyzer")
+@app.route("/emotionDetector")
 def sent_analyzer(): 
     # Retrieve the text to analyze from the request arguments 
     text_to_analyze = request.args.get('textToAnalyze')
@@ -12,8 +12,8 @@ def sent_analyzer():
     response = sentiment_analyzer(text_to_analyze)
 
     # Extract the label and score from the response 
-    label = response['label'] 
-    score = response['score']
+    label = response['documentSentiment']['label'] 
+    score = response['documentSentiment']['score']
 
     # Return a formatted string with the sentiment label and score 
     return "The given text has been identified as {} with a score of {}.".format(label.split('_')[1], score)
@@ -21,4 +21,4 @@ def sent_analyzer():
 @app.route("/") 
 def render_index_page(): return render_template('index.html')
 
-if __name__ == "__main__": app.run(host="0.0.0.0", port=5000)
+if __name__ == "__main__": app.run(debug=True)
