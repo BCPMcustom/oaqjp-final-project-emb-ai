cf87afbd7137a2057dc143d482626048cd6b2e61
BCPMcustom <bcpmcustomphone@gmail.com>
2026-08-04 19:33:18 -0400

    rename task2.py for final submission

---
+++ task2.py
@@ -0,0 +1,21 @@
+'''
+This module connects the server to the IBM API
+'''
+import json
+import requests
+
+
+def sentiment_analyzer(text_to_analyse):
+    '''
+    This function connects to the API and check the connection status
+    '''
+    url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
+    myobj = { "raw_document": { "text": text_to_analyse } }
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}  
+    response = requests.post(url, json = myobj, headers=header, timeout=10)
+
+
+    # Parse the response from the API
+    formatted_response = json.loads(response.text)
+
+    return formatted_response
