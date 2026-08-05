d17e48203b2d200187aaecc57b351627b4659c57
BCPMcustom <bcpmcustomphone@gmail.com>
2026-08-05 15:19:11 -0400

    Task 2 complete

--- task2.py
+++ task2.py
@@ -9,8 +9,7 @@ def emotion_detector(text_to_analyse):
     '''
     This function connects to the API and check the connection status
     '''
-    url = "https://sn-watson-emotion.labs.skills.network/v1/\
-    watson.runtime.nlp.v1/NlpService/EmotionPredict"
+    url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
     myobj = { "raw_document": { "text": text_to_analyse } }
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     response = requests.post(url, json = myobj, headers=header, timeout=10)
